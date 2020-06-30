1b70a6e2dd977c9d4dac74127fbd22d7ed0c5262
jourm <jourm91@gmail.com>
2020-06-30 13:30:38 +0200

    Split data, set up algorithm and hyperparameters

--- main.py
+++ main.py
@@ -19,4 +19,22 @@ del df['Regionname']
 del df['Propertycount']
 
 df.dropna(axis=0, how='any', thresh=None, subset=None, inplace=True)
-df.
+df = pd.get_dummies(df, columns = ['Suburb', 'CouncilArea', 'Type'])
+x = df.drop('Price',axis=1)
+y = df['Price']
+
+# Split Data
+X_train, X_test, y_train, y_test = test_train_split(X, y, test_size=0.3, shuffle=True)
+
+# Algorithm and hyperparameters
+model = ensemble.GradientBoostingRegressor(
+    n_estimators = 150,
+    learning_rate = 0.1,
+    max_depth = 30,
+    min_samples_split = 4,
+    min_samples_leaf = 6,
+    max_features = 0.6,
+    loss = 'huber'
+)
+
+model.fit(X_train, y_train)
